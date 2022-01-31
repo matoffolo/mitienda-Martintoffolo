c7e9f3dd85a0accd4895afd24e4f910faf055345
Martin Toffolo <martin.toffolo@gmail.com>
2022-01-30 21:28:21 -0300

    cambios itemcounter

--- src/Componentes/ItemListContainer/ItemCount.js
+++ src/Componentes/ItemListContainer/ItemCount.js
@@ -2,15 +2,22 @@ import React, {useState} from 'react';
 
 const ItemCount = () => {
     const [counter, setCounter] = useState (0)
-
+    const stock = 20
+    const inicial = 0
     const incrementarContador = () => {
-      setCounter(counter +1)
+        if(counter<stock){
+        setCounter(counter +1)}
     }
+    const decrementarContador = () => {
+        if(counter>inicial){
+        setCounter(counter - 1)}
+    }
+
   return (
     <div>
       <p>{counter}</p>
     <button onClick={incrementarContador}>Incrementar</button>
-    <button onClick={()=> setCounter(counter-1)}>Decrementar</button>
+    <button onClick={decrementarContador}>Decrementar</button>
     </div>
   );
 }
